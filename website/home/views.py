847f6922b5e9aaf46dd5fa84e9d1ca29b78bbb60
Asmit Paul <hiasmitpaul@gmail.com>
2025-08-16 22:35:39 +0530

    added home page and connected it to the prediction form page

--- website/home/views.py
+++ website/home/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import render
 
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("Welcome")
+    return render(request, "home.html")
