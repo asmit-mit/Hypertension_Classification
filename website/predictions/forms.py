4c157807f90464964aed7bed2ef3467514581d09
Asmit Paul <hiasmitpaul@gmail.com>
2025-08-25 14:00:30 +0530

    made models better and fixed website buttons
    - feature engineered more data to make models better
    - fixed post form processing to account for the new features
    - fixed the website button from shrinking unpredictably

--- website/predictions/forms.py
+++ website/predictions/forms.py
@@ -2,11 +2,11 @@ from django import forms
 
 
 class PredictionForm(forms.Form):
-    age = forms.IntegerField(label="Age")
+    age = forms.IntegerField(label="Age", max_value=100, min_value=0)
     salt_intake = forms.FloatField(label="Salt Intake (grams/day)")
-    stress_score = forms.FloatField(label="Stress Score (0-10)")
-    sleep_duration = forms.FloatField(label="Sleep Duration (hours/day)")
-    bmi = forms.FloatField(label="Body Mass Index (BMI)")
+    stress_score = forms.FloatField(label="Stress Score (0-10)", max_value=10, min_value=0)
+    sleep_duration = forms.FloatField(label="Sleep Duration (hours/day)", max_value=24, min_value=0)
+    bmi = forms.FloatField(label="Body Mass Index (BMI)", min_value=0, max_value=100)
 
     family_history = forms.ChoiceField(
         choices=[("Yes", "Yes"), ("No", "No")], label="Family History of Hypertension"
